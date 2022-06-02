7fd81167099e877a7403b75a000602f4c17745d8
cheapskate01 <andrewrobinson615@gmail.com>
2022-06-01 20:30:29 -0500

    now able to pass the data in our dat files to javascript

--- PlaylistData.js
+++ PlaylistData.js
@@ -1,11 +1,12 @@
 
 function loadFile() {
 	const request = new XMLHttpRequest();
+	request.responseType = "text";
 	request.addEventListener("load", parseOurPlaylist);
 	request.open("GET", "./usrdata/lists/testlist.dat");
 	request.send();
 }
 
 function parseOurPlaylist() {
-	alert("fuck you");
+	alert(this.responseText);
 }
