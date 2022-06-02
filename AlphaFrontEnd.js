f8d54ef7a8d39f10d061c98a222badd59f3fa64a
thespaceburrito <itsjonathanobi@gmail.com>
2022-06-02 16:13:44 -0500

    improved the ui

--- AlphaFrontEnd.js
+++ AlphaFrontEnd.js
@@ -8,9 +8,29 @@ function validateLink(myURL) {
 
 function getLink() {
     if (validateLink(document.getElementById("form__field").value)){
-       
+        Hide("container__item");
+        Show("loader");
+        setTimeout(function() { Hide("loader"); Show("container__item2");}, 1500);
+        
+        
     }else{
         document.getElementById('error').innerHTML = "Please enter a valid playlist url";
     }
    
 }
+
+var hideId;
+function Hide(hideId) {
+    var h = document.getElementById(hideId);
+    if (!(h.style.display === "none")) {
+        h.style.display = 'none';
+    } 
+ }
+
+ var showId;
+ function Show(showId) {
+     var s = document.getElementById(showId);
+     if (s.style.display === "none") {
+        s.style.display = 'inherit';
+     }
+ }
