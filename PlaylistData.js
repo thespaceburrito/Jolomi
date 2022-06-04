2a5c40ff3e89f413963861ea743076971216ded4
cheapskate01 <andrewrobinson615@gmail.com>
2022-06-04 11:18:02 -0500

    can now load the isrc's from our dat file into arrays with no separators

--- PlaylistData.js
+++ PlaylistData.js
@@ -8,5 +8,20 @@ function loadFile() {
 }
 
 function parseOurPlaylist() {
-	alert(this.responseText);
+	let playlist = this.responseText;
+	let len = playlist.length;
+	let songnum = (~~(len/12));
+	let songs = new Array();
+
+	//alert(songnum);
+
+	for (let i=0; i<songnum; i++) {
+		let offset = i * 12;
+		songs.push(playlist.slice((offset+0),(offset+12)));
+	}
+
+	for (let i = 0; i < songs.length; i++) {
+		alert(songs[i]);
+	}
+	
 }
